766766c26caddbe14246a52ac2f3e3282c1fc1d8
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-25 10:53:21 +0000

    initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+        // java is an OO language
+        // No code can be run in a Java that's not in a class
+        // Java is statically typed
+
+        Cat cat = new Cat();
+    }
+}
